3d6e166371eb633498ef8dc4694e279511ec8d0e
Ramkumar Shah <ramkumar46672@gmail.com>
2026-01-21 15:07:53 +0530

    Add files via upload

---
+++ funquary.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+print("Program started...")
+
+try:
+    print("Trying to connect...")
+
+    conn = mysql.connector.connect(
+        host="localhost",  # Force IPv4
+        user="root",
+        password="your",
+        database="my_database",
+        port=3306,
+        auth_plugin="mysql_native_password",
+        use_pure=True,
+        connection_timeout=5
+    )
+
+    print("Connection object created")
+
+    if conn.is_connected():
+        print("Connected Successfully")
+    else:
+        print("Not Connected")
+    cursor = conn.cursor()
+    def sqlquary():
+        
+
+        # Database create query
+        quary =input("Enter SQL Quary :")
+        cursor.execute(quary)
+    
+    sqlquary()
+
+
+    conn.close()
+    print("Connection closed")
+
+except Exception as e:
+    print("ERROR:", e)
+
